7473f9aae77a1a5ec127333e08781bda7ee3457f
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-02-20 19:09:35 +0100

    did some clean up

---
+++ sweeper project/Assets/Scripts/Enums.cs
@@ -0,0 +1,31 @@
+public enum EventType
+{
+    // gameplay related
+    BOMB_UPDATE,        // passes int
+    UPDATE_TIME,        // passes float
+    START_POS,          // passes vector3
+    PLANT_FLAG,         // passes vector3[]
+    ADD_EMPTY,          // passes gameobject
+    REMOVE_FLAG,        // passes gameobject
+    ADD_GOOD_TILE,      // passes gameobject
+    COUNT_BOMBS,        // passes nothing  
+    PICK_TILE,          // passes nothing
+    PLAY_CLICK,         // passes nothing
+    PLAY_FLAG,          // passes nothing
+    TILE_CLICK,         // passes nothing
+    // gameflow related
+    PREPARE_GAME,       // passes nothing
+    RANDOM_GRID,        // passes nothing
+    RESET_GAME,         // passes nothing
+    START_GAME,         // passes nothing
+    END_GAME,           // passes nothing
+    WIN_GAME,           // passes nothing
+    GAME_LOSE,          // passes nothing
+    // input related
+    INPUT_LEFT,         // passes nothing
+    INPUT_RIGHT,        // passes nothing
+    INPUT_UP,           // passes nothing
+    INPUT_DOWN,         // passes nothing
+    INPUT_FORWARD,      // passes nothing
+    INPUT_BACK,         // passes nothing
+}
--- sweeper project/Assets/Scripts/EventSystem.cs
+++ sweeper project/Assets/Scripts/EventSystem.cs
@@ -1,40 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public enum EventType
-{
-    // gameplay related
-    COUNT_BOMBS     = 0,    // passes nothing    
-    SHOW_BOMBS      = 1,    // not used
-    END_GAME        = 2,    // passes nothing
-    PICK_TILE       = 3,    // passes nothing
-    START_GAME      = 4,    // passes nothing
-    PREPARE_GAME    = 5,    // passes nothing
-    PLANT_FLAG      = 6,    // passes vector 3[]
-    REMOVE_FLAG     = 7,    // passes gameobject
-    RESET_GAME      = 8,    // passes nothing
-    FIRST_CLICK     = 10,   // not used
-    ADD_EMPTY       = 11,   // passes gameobject
-    START_POS       = 12,   // passes vector 3
-    WIN_GAME        = 13,   // passes nothing
-    BOMB_UPDATE     = 14,   // passes int
-    // input related
-    INPUT_LEFT      = 15,   // passes nothing
-    INPUT_RIGHT     = 16,   // passes nothing
-    INPUT_UP        = 17,   // passes nothing
-    INPUT_DOWN      = 18,   // passes nothing
-    INPUT_FORWARD   = 19,   // passes nothing
-    INPUT_BACK      = 20,   // passes nothing
-    // new stuff
-    RANDOM_GRID     = 21,   // passes nothing
-    GAME_LOSE       = 22,   // passes nothing
-    ADD_GOOD_TILE   = 23,   // passes gameobject
-    UPDATE_TIME     = 24,   // passes float
-    PLAY_CLICK      = 25,   // passes nothing
-    PLAY_FLAG       = 26,   // passes nothing
-    TILE_CLICK      = 27,   // passes nothing
-}
-
 // event system without arguments
 public static class EventSystem
 {
