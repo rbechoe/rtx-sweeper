41521d338b60430c275b9fbb6721a21be86847cf
Ravi Bechoe <ravibechoe@outlook.com>
2022-02-24 16:17:44 +0100

    UI fixes

--- sweeper project/Assets/Scripts/Tutorial/Dialogues.cs
+++ sweeper project/Assets/Scripts/Tutorial/Dialogues.cs
@@ -11,6 +11,7 @@ public class Dialogues : MonoBehaviour
 
     public TextMeshProUGUI text;
     public GameObject dialogueBar;
+    public GameObject[] uiObjects;
 
     public float startDelay, endDelay;
     private float totalWaitTime;
@@ -18,6 +19,7 @@ public class Dialogues : MonoBehaviour
     void Start()
     {
         DisableBar();
+        DisableUIElements();
         StartCoroutine(DelayedMethods.FireMethod(EnableBar, startDelay));
         StartCoroutine(DelayedMethods.FireMethod(RandomizeGrid, 10));
         totalWaitTime += startDelay - waitTimes[0];
@@ -29,6 +31,7 @@ public class Dialogues : MonoBehaviour
         }
 
         StartCoroutine(DelayedMethods.FireMethod(DisableBar, totalWaitTime + endDelay));
+        StartCoroutine(DelayedMethods.FireMethod(EnableUIElements, totalWaitTime));
     }
 
     void EnableBar()
@@ -44,6 +47,23 @@ public class Dialogues : MonoBehaviour
     void Dialogue(int line)
     {
         text.text = lines[line];
+    }
+
+    // TODO detach all below this comment and place it in a tutorial manager
+    void DisableUIElements()
+    {
+        foreach (GameObject ui in uiObjects)
+        {
+            ui.SetActive(false);
+        }
+    }
+
+    void EnableUIElements()
+    {
+        foreach (GameObject ui in uiObjects)
+        {
+            ui.SetActive(true);
+        }
     }
 
     public void RandomizeGrid()
