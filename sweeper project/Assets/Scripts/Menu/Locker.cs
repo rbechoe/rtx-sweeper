c719fe292de305b6ee700543e4f8491bac5279e7
Ravi Bechoe <ravibechoe@outlook.com>
2022-03-10 16:20:43 +0100

    bug fix

--- sweeper project/Assets/Scripts/Menu/Locker.cs
+++ sweeper project/Assets/Scripts/Menu/Locker.cs
@@ -6,11 +6,27 @@ public class Locker : MonoBehaviour
 {
     public GameObject locked, unlocked;
 
+    public bool delayed;
+
     private void Start()
+    {
+        if (delayed)
+        {
+            DelayedMethods.FireMethod(DelayedLocker, 1);
+        }
+        else
+        {
+            locked.SetActive(true);
+            unlocked.SetActive(false);
+        }
+    }
+
+    private void DelayedLocker()
     {
         locked.SetActive(true);
         unlocked.SetActive(false);
     }
+
     public void UnlockAreas()
     {
         locked.SetActive(false);
