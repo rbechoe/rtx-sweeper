a7e537482ca5b0da2f1266c59af2f8d7da92da59
Ravi Bechoe <ravibechoe@outlook.com>
2022-03-24 18:06:47 +0100

    fixed post processing related stuff

--- sweeper project/Assets/Scripts/Menu/MenuManager.cs
+++ sweeper project/Assets/Scripts/Menu/MenuManager.cs
@@ -57,11 +57,12 @@ public class MenuManager : MonoBehaviour
         // disable interactables based on savedata
         dataSerializer = gameObject.GetComponent<DataSerializer>();
         AccountData accountData = dataSerializer.GetUserData();
-        if (accountData.tutorialVictories > 0) playBtn.interactable = true;
-        if (accountData.arcticVictories > 0) asiaBtn.interactable = true;
-        else asiaBtn.interactable = false;
-        if (accountData.asiaVictories > 0) desertBtn.interactable = true;
-        else desertBtn.interactable = false;
+        if (accountData.tutorialVictories > 0) playBtn.gameObject.SetActive(true);
+        else playBtn.gameObject.SetActive(false);
+        if (accountData.arcticVictories > 0) asiaBtn.gameObject.SetActive(true);
+        else asiaBtn.gameObject.SetActive(false);
+        if (accountData.asiaVictories > 0) desertBtn.gameObject.SetActive(true);
+        else desertBtn.gameObject.SetActive(false);
         if (accountData.desertVictories > 0) bossBtn.gameObject.SetActive(true);
         else bossBtn.gameObject.SetActive(false);
 
