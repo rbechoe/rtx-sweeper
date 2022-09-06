3d7cab6c8177307568c86e368300ec10a67a9048
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-09-07 01:07:09 +0200

    some more steps done

--- sweeper project/Assets/Scripts/TranslationTool/LanguageDatabase.cs
+++ sweeper project/Assets/Scripts/TranslationTool/LanguageDatabase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,5 +26,57 @@ public class LanguageDatabase : ScriptableObject
         }
 
         return string.Empty;
+    }
+
+    public List<string> GetSentences(string ID)
+    {
+        for (int i = 0; i < translations.Count; i++)
+        {
+            if (translations[i].ID == ID)
+            {
+                Translate translate = translations[i];
+                return translate.sentence;
+            }
+        }
+
+        return new List<string>();
+    }
+
+    public void AddEntry(string textInput)
+    {
+        List<string> temp = new List<string>();
+        int count = Enum.GetNames(typeof(Language)).Length;
+
+        for (int i = 0; i < count; i++)
+        {
+            temp.Add("");
+        }
+
+        Translate translate = new Translate(textInput, temp);
+        translations.Add(translate);
+    }
+
+    public void DeleteEntry(string ID)
+    {
+        for (int i = 0; i < translations.Count; i++)
+        {
+            if (translations[i].ID == ID)
+            {
+                translations.Remove(GetTranslateObject(ID));
+            }
+        }
+    }
+
+    public Translate GetTranslateObject(string ID)
+    {
+        for (int i = 0; i < translations.Count; i++)
+        {
+            if (translations[i].ID == ID)
+            {
+                return translations[i];
+            }
+        }
+
+        return null;
     }
 }
